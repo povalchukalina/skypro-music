d46cc6494fc3028ec6b31ae2efd3bc6d65003f61
povalchukalina <138883858+povalchukalina@users.noreply.github.com>
2023-10-23 12:58:37 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+          <header className="App_Name"> 
+              Здесь скоро будет Skypro.Music
+          </header>
+      </div>
+  );
+}
+
+export default App;
